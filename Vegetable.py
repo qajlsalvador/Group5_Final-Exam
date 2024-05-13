62f48ce5a6e0b2c5a7f12ae3acd44737cff16b17
Andreu Salvador <119837660+qajlsalvador@users.noreply.github.com>
2024-05-13 22:22:14 +0800

    Update and rename flower.py to Vegetable.py

--- Vegetable.py
+++ Vegetable.py
@@ -8,7 +8,7 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-	model = tf.keras.models.load_model('./flower_model_trained.hdf5')
+	model = tf.keras.models.load_model('./VStrained_modelA.h5')
 	return model
 
 
