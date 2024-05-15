a31babb6bf6638323fabca292f8ebda43a85a486
Andreu Salvador <119837660+qajlsalvador@users.noreply.github.com>
2024-05-16 03:26:50 +0800

    Update Vegetable.py

--- Vegetable.py
+++ Vegetable.py
@@ -63,5 +63,5 @@ def set_background_image(image_url):
         )
 
 # Your image from GitHub
-bg_image_url = "https://github.com/qajlsalvador/Group5_Final-Exam/blob/main/background.jpg"
+bg_image_url = "https://raw.githubusercontent.com/qajlsalvador/Group5_Final-Exam/main/background.jpg"
 set_background_image(bg_image_url)
